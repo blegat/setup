0a138845ec1dff7d272e128f8494c51f8b7cb4d9
Benoît Legat <benoit.legat@gmail.com>
2026-06-12 08:51:10 +0200

    Use JuMP logo

--- bin/setup.sh
+++ bin/setup.sh
@@ -6,6 +6,13 @@ source "$SCRIPT_DIR/lib.sh"
 link "Register custom.sh as omarchy post-update hook" \
   "$SCRIPT_DIR/custom.sh" "$HOME/.config/omarchy/hooks/post-update.d"
 
+reset_default "Clear default omarchy screensaver branding" \
+  "$HOME/.config/omarchy/branding/screensaver.txt" \
+  "$HOME/.local/share/omarchy/logo.txt"
+
+link "Use JuMP logo as screensaver" \
+  "$ROOT_SRC/logo/JuMP_logo.txt" "$HOME/.config/omarchy/branding/screensaver.txt"
+
 ensure_line "Source profile.sh from ~/.bashrc" \
   "$HOME/.bashrc" "source $ROOT_SRC/profile.sh"
 
---
+++ logo/README.md
@@ -0,0 +1,5 @@
+Obtained with:
+```sh
+omarchy-transcode-ascii JuMP_logo.svg JuMP_logo.txt
+Wrote ASCII art to JuMP_logo.txt
+```
